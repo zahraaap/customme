dd77a7243c1b35002900f962d6eaa24fccb80881
zahra <zahrapadash1377@gmail.com>
2024-10-17 05:57:40 -0700

    Risponsive the GiftCard component

--- src/components/GiftCard.jsx
+++ src/components/GiftCard.jsx
@@ -3,19 +3,19 @@ import cn from "classnames";
 import LinkButton from "./ui/LinkButton";
 
 const GiftCard = ({ image, title, link }) => (
-  <div className="w-full lg:w-1/2 bg-ac-tint-300 flex rounded-2xl">
-    <div className="flex flex-col gap-y-6 py-2 px-4 lg:p-6">
-      <h4 className="body-5 lg:heading-4 text-gray-950">{ title }</h4>
+  <div className="w-full lg:w-1/2 bg-ac-tint-300 flex justify-between rounded-2xl">
+    <div className="flex flex-col items-cente justify-between md:justify-center gap-y-1 md:gap-y-6 py-2 pl-2 pr-4 md:px-4 lg:p-6">
+      <h4 className="body-5 md::heading-4 text-gray-950">{ title }</h4>
       <LinkButton variant="outline"
                   size="sm"
                   text="دیدن محصولات"
-                  className="self-start"
+                  className="md:self-start"
                   href={link} />
     </div>
     <img src={`/images/home/${image}`}
          alt={title}
          className={cn(
-           "w-2/5 lg:w-1/2 h-full object-cover object-center rounded-l-2xl"
+           "w-2/5 md:w-1/2 max-h-24 md:max-h-48 h-full object-cover object-center rounded-l-2xl lg:rounded-r-none"
          )}
     />
   </div>
